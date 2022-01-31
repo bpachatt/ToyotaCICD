61db68ed7d2f613fb1df2dc7488f779d6937428a
Bivin Pachatt (Vidyatech Software LLC) <v-bipach@microsoft.com>
2022-01-30 18:57:26 -0800

    new solution created

---
+++ test/ToyotaWeb.Test/UnitTest1.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+
+namespace ToyotaWeb.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+
+    [Fact]
+    public void Test2()
+    {
+
+    }
+
+    [Fact]
+    public void Test3()
+    {
+
+    }
+}
